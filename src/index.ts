96f75b878f650b19defd975877b61eaa5a2ee847
Bunniesnu <bunniesnu@gmail.com>
2026-02-03 02:27:33 +0000

    Add REDIRECT_URL to env, change to redirect response

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,6 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		return Response.redirect(env.REDIRECT_URL, 301);
 	},
 } satisfies ExportedHandler<Env>;
